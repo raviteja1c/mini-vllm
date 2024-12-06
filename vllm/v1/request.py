c4eecda28b742bb1cc794acd031ff06ce66cf78d
raviteja <112757229+raviteja1c@users.noreply.github.com>
2024-12-06 12:45:12 +0530

    AdapterRequest LoRARequest

--- vllm/v1/request.py
+++ vllm/v1/request.py
@@ -1,4 +1,5 @@
 import enum
 from typing import List, Optional, Union
 
-from vllm.inputs import DecoderOnlyInputs, SingletonInputsAdapter
+from vllm.inputs import DecoderOnlyInputs, SingletonInputsAdapter, token_inputs
+from vllm.lora.request import LoRARequest
